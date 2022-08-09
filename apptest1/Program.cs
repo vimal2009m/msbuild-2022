9a2017f56f676c77b839330aa8545b586f5b96cc
vimal <vimal@gmail.com>
2022-08-08 20:51:23 -0700

    my project completed

---
+++ apptest1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace apptest1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World Test!");
+        }
+    }
+}
